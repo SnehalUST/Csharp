aedd00c15ec6ef05d07d98b2be6b4a0cfe5cb795
SnehalUST <snehal.hande@ust.com>
2024-07-18 23:03:50 +0530

    Print Messgae task

---
+++ Project_1/MsgBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    internal class MsgBox
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, UST!");
+            Console.WriteLine("This is 2 program");
+        }
+    }
+}
---
+++ Project_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_1
+{
+    internal class Program
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
